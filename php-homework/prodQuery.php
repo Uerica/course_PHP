e79672cccab6e602cc4bf8e12da3cde2ea74f1b7
uerica <minas19901231@gmail.com>
2019-06-05 08:52:03 +0800

    add hw

---
+++ php-homework/prodQuery.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $errMsg = "";
+    try {
+         $dsn = "mysql:host=localhost;port=3306;dbname=books;charset=utf8";
+         $user = "root";
+         $password = "root";
+         $options = array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => PDO::CASE_NATURAL);
+         $pdo = new PDO($dsn, $user, $password, $options);
+         $sql = "select * from products where psn='{$_POST["psn"]}'";
+         $pdoStatement = $pdo->query($sql);
+ 
+       
+    } catch (PDOException $e) {
+         $errMsg .=  $e->getMessage(). "<br>";	
+         $errMsg .=  $e->getLine(). "<br>";		
+    }
+ 
+   ?>
+ 
+   
+ 
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <title>Document</title>
+ </head>
+ <body>
+ 
+ <?
+ 
+ 
+ if($errMsg !=""){
+       exit($errMsg);
+    }
+ ?>  	
+ 
+ <table border='1' cellspacing='0' align='center'>";
+ 
+ <php?
+         while($row = $pdoStatement->fetch(PDO::FETCH_ASSOC)){
+ ?>
+ <!-- 在此結束改用html -->
+ 
+          <tr>
+          <td><?php echo $row["psn"];?></td>
+          <td><?php echo $row["pname"];?></td>
+          <td><?php echo $row["price"];?></td>
+          <td><?php echo $row["author"];?></td>    
+          </tr>
+ <?php
+        }
+  ?>
+ 
+ </table>
+</body>
+</html>
