d77ce481b9f8d0267fcf75d2db16c7f2db4a38ff
Uerica <minas19901231@gmail.com>
2019-07-12 15:48:45 +0800

    add teacherDemo cart

---
+++ teaher-demo/cart_C/cartUpdate.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+$psn = $_REQUEST["psn"];
+
+if( isset($_REQUEST["btnUpdate"])===true){//修改
+	$_SESSION["qty"][$psn]=$_REQUEST["qty"];	
+}else{ //刪除
+	unset($_SESSION["pname"][$psn]);
+	unset($_SESSION["price"][$psn]);
+	unset($_SESSION["qty"][$psn]);
+}
+
+header("location:cartShow.php");
+?>
